742cb1531611aee8b4cdc528a0750d7dd32a85d5
MasterShifu <kasparbergert6@gmail.com>
2024-11-26 12:49:56 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-
-const Hello = (props) => {
-  /*
-  const age = props.age
-  const name = props.name
-  */
-
-  const {name, age} = props;
-  console.log(props);
-
-  //returns the year you were born in.
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = ({counter}) => {
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Kaspar'
-  const age = 277
-
-  return (
-    <div>
-      <h1>Tere Kaspar</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
