a8d0dc3c18b29f85c450e6818ef296a2496e5c91
MasterShifu <kasparbergert6@gmail.com>
2024-11-26 15:00:45 +0200

    unicafe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import './App.css'
 
 
 
@@ -11,7 +11,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   return (
-    <div>
+    <div id="container">
       <h1>Give feedback</h1>
 
       <button onClick={() => {setGood(good+1)}}>good</button>
@@ -20,12 +20,32 @@ const App = () => {
 
       <h1>statistics</h1>
 
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good+neutral+bad}</p>
-      <p>average: {(good+neutral+bad) / 3}</p>
-      <p>positive: {(good/(good+neutral+bad)) * 100}%</p>
+    <table>
+      <tr>
+      <td>Good</td>
+      <td>{good}</td>
+    </tr>
+    <tr>
+      <td>Neutral</td>
+      <td>{neutral}</td>
+    </tr>
+    <tr>
+      <td>Bad</td>
+      <td>{bad}</td>
+    </tr>
+    <tr>
+      <td>All (Total)</td>
+      <td>{good + neutral + bad}</td>
+    </tr>
+    <tr>
+      <td>Average Feedback</td>
+      <td>{Math.round((good + neutral + bad) / 3)}</td>
+    </tr>
+    <tr>
+      <td>Positive Feedback: </td>
+      <td>{Math.round((good / (good + neutral + bad)) * 100)}%</td>
+    </tr>
+      </table>
     </div>
   )
 }
