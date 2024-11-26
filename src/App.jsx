9f0211aa7b631b7b0020ae2a75d2c05b1a813c64
MasterShifu <kasparbergert6@gmail.com>
2024-11-26 14:32:18 +0200

    give feedback completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import { useState } from 'react'
 
+
+
+
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
 
-  const handleLeftClick = () => {
-    clicks.left += 1;
-    setClicks(clicks)
-  }
-
-  const handleRightClick = () => {
-    clicks.right += 1;
-    setClicks(clicks)
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      <h1>Give feedback</h1>
+
+      <button onClick={() => {setGood(good+1)}}>good</button>
+      <button onClick={() => {setNeutral(neutral+1)}}>neutral</button>
+      <button onClick={() => {setBad(bad+1)}}>bad</button>
+
+      <h1>statistics</h1>
+
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {good+neutral+bad}</p>
+      <p>average: {(good+neutral+bad) / 3}</p>
+      <p>positive: {(good/(good+neutral+bad)) * 100}%</p>
     </div>
   )
 }
